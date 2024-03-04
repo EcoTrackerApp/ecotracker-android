f1bc32f5966a51546607786b1b99e6cef8cada1e
VIAL Sébastien <sebvi66@hotmail.fr>
2024-03-04 10:17:16 +0100

    fix: ça cook

--- app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
@@ -1,15 +1,18 @@
 package fr.umontpellier.carbonalyser
 
+import android.app.usage.NetworkStats
+import android.app.usage.NetworkStatsManager
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import fr.umontpellier.carbonalyser.ui.theme.CarbonalyserTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +22,32 @@ class MainActivity : ComponentActivity() {
             CarbonalyserTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Button(
+                        onClick = {
+                            val networkStatsManager =
+                                applicationContext.getSystemService(NetworkStatsManager::class.java)
+                            val networkType = ConnectivityManager.TYPE_WIFI
+                            val subscriberId = "" // Subscriber id, usually empty for WIFI
+
+                            val networkStats = networkStatsManager.querySummary(
+                                networkType,
+                                subscriberId,
+                                0,
+                                System.currentTimeMillis()
+                            )
+
+                            Log.i("Carbonalyzer", "Purée")
+                            while (networkStats.hasNextBucket()) {
+                                val bucket = networkStats.getNextBucket(NetworkStats.Bucket())
+                                Log.i("Carbonalyzer", "Entry: $bucket")
+                            }
+                        },
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Text("Debug")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true) @Preview
-@Composable
-fun GreetingPreview() {
-    CarbonalyserTheme {
-        Greeting("Android")
     }
 }
