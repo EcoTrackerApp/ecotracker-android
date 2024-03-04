b011c3c57c4eefc72a82b9ac8564e5ad73539c2c
VIAL Sébastien <sebvi66@hotmail.fr>
2024-03-04 20:51:04 +0100

    feat: working app display

--- app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
@@ -3,7 +3,6 @@ package fr.umontpellier.carbonalyser
 import android.app.AppOpsManager
 import android.app.usage.NetworkStats
 import android.app.usage.NetworkStatsManager
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
@@ -20,7 +19,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import fr.umontpellier.carbonalyser.ui.theme.CarbonalyserTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,44 +41,8 @@ class MainActivity : ComponentActivity() {
     fun DebugButton() {
         Button(
             onClick = {
-                val packageManager = applicationContext.packageManager
-                val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
-                val mode = appOps.unsafeCheckOpNoThrow(
-                    AppOpsManager.OPSTR_GET_USAGE_STATS,
-                    Process.myUid(), packageName
-                )
-                if (mode != AppOpsManager.MODE_ALLOWED) {
-                    val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
-                    startActivity(intent)
-                    return@Button
-                }
-                val networkStatsManager = getSystemService(NETWORK_STATS_SERVICE) as NetworkStatsManager
-                val networkType = ConnectivityManager.TYPE_WIFI
-                val subscriberId = ""
-
-                try {
-                    val networkStats: NetworkStats = networkStatsManager.querySummary(
-                        networkType,
-                        subscriberId,
-                        0,
-                        System.currentTimeMillis()
-                    )
-
-                    while (networkStats.hasNextBucket()) {
-                        val bucket = NetworkStats.Bucket()
-                        networkStats.getNextBucket(bucket)
-                        val uid = bucket.uid
-                        val rxBytes = bucket.rxBytes
-                        val txBytes = bucket.txBytes
-                        val packages = getAppNameFromUID(applicationContext, uid)
-
-                        Log.i(
-                            "NetworkStats",
-                            "App: ${packages}, RX Bytes: $rxBytes, TX Bytes: $txBytes"
-                        )
-                    }
-                } catch (e: SecurityException) {
-                    Log.e("NetworkStats", "Permission Denied", e)
+                lifecycleScope.launch {
+                    fetchNetworkStats()
                 }
             },
         ) {
@@ -83,21 +50,55 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun getAppNameFromUID(context: Context, uid: Int): String {
-        val packageManager = context.packageManager
-        val packages = packageManager.getPackagesForUid(uid)
+    private suspend fun fetchNetworkStats() {
+        withContext(Dispatchers.IO) {
+            val packageManager = applicationContext.packageManager
+            val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
+            val mode = appOps.unsafeCheckOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                Process.myUid(), packageName
+            )
+            if (mode != AppOpsManager.MODE_ALLOWED) {
+                val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+                startActivity(intent)
+                return@withContext
+            }
+            val networkStatsManager = getSystemService(NETWORK_STATS_SERVICE) as NetworkStatsManager
+            val networkType = ConnectivityManager.TYPE_WIFI
+            val subscriberId = ""
 
-        if (packages != null && packages.isNotEmpty()) {
-            val packageName = packages[0]
             try {
-                val packageInfo = packageManager.getPackageInfo(packageName, 0)
-                return packageInfo.applicationInfo.loadLabel(packageManager).toString()
-            } catch (e: PackageManager.NameNotFoundException) {
-                e.printStackTrace()
+                val networkStats: NetworkStats = networkStatsManager.querySummary(
+                    networkType,
+                    subscriberId,
+                    0,
+                    System.currentTimeMillis()
+                )
+
+                while (networkStats.hasNextBucket()) {
+                    val bucket = NetworkStats.Bucket()
+                    networkStats.getNextBucket(bucket)
+                    val uid = bucket.uid
+                    val rxBytes = bucket.rxBytes
+                    val txBytes = bucket.txBytes
+                    val applicationInfo = packageManager.getPackagesForUid(uid)
+                        ?.map {
+                            packageManager.getPackageInfo(
+                                it,
+                                PackageManager.GET_META_DATA or PackageManager.MATCH_UNINSTALLED_PACKAGES
+                            )
+                        }
+                        ?.map { packageManager.getApplicationLabel(it.applicationInfo) }
+
+                    Log.i(
+                        "NetworkStats",
+                        "App: ${applicationInfo}, RX Bytes: $rxBytes, TX Bytes: $txBytes"
+                    )
+                }
+            } catch (e: SecurityException) {
+                Log.e("NetworkStats", "Permission Denied", e)
             }
         }
-
-        // Si aucune application correspondante n'a été trouvée, vous pouvez renvoyer une chaîne vide ou un message d'erreur
-        return ""
     }
+
 }
