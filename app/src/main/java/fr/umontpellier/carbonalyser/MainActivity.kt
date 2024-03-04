5b5b3c2ecf345a6d5681b721890aabcfb63505b1
VIAL Sébastien <sebvi66@hotmail.fr>
2024-03-04 14:34:12 +0100

    feat: display app name

--- app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
@@ -3,7 +3,9 @@ package fr.umontpellier.carbonalyser
 import android.app.AppOpsManager
 import android.app.usage.NetworkStats
 import android.app.usage.NetworkStatsManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Process
@@ -36,6 +38,7 @@ class MainActivity : ComponentActivity() {
     fun DebugButton() {
         Button(
             onClick = {
+                val packageManager = applicationContext.packageManager
                 val appOps = getSystemService(APP_OPS_SERVICE) as AppOpsManager
                 val mode = appOps.unsafeCheckOpNoThrow(
                     AppOpsManager.OPSTR_GET_USAGE_STATS,
@@ -64,10 +67,11 @@ class MainActivity : ComponentActivity() {
                         val uid = bucket.uid
                         val rxBytes = bucket.rxBytes
                         val txBytes = bucket.txBytes
+                        val packages = getAppNameFromUID(applicationContext, uid)
 
                         Log.i(
                             "NetworkStats",
-                            "UID: $uid, RX Bytes: $rxBytes, TX Bytes: $txBytes"
+                            "App: ${packages}, RX Bytes: $rxBytes, TX Bytes: $txBytes"
                         )
                     }
                 } catch (e: SecurityException) {
@@ -77,5 +81,23 @@ class MainActivity : ComponentActivity() {
         ) {
             Text("Debug Network")
         }
+    }
+
+    fun getAppNameFromUID(context: Context, uid: Int): String {
+        val packageManager = context.packageManager
+        val packages = packageManager.getPackagesForUid(uid)
+
+        if (packages != null && packages.isNotEmpty()) {
+            val packageName = packages[0]
+            try {
+                val packageInfo = packageManager.getPackageInfo(packageName, 0)
+                return packageInfo.applicationInfo.loadLabel(packageManager).toString()
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+            }
+        }
+
+        // Si aucune application correspondante n'a été trouvée, vous pouvez renvoyer une chaîne vide ou un message d'erreur
+        return ""
     }
 }
