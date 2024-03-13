b7b3792efd36dde5be9c29c2edf41fe889b7853b
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-03-13 15:13:53 +0100

    tile emission finish

--- app/src/main/java/fr/umontpellier/carbonalyser/ui/components/header/Header.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/ui/components/header/Header.kt
@@ -6,15 +6,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme.colorScheme
-import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -22,6 +18,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.umontpellier.carbonalyser.R
+import fr.umontpellier.carbonalyser.ui.theme.CarbonalyserTheme
 
 @Composable
 fun Header() {
@@ -29,17 +26,11 @@ fun Header() {
     var blueColor = 0xFF1a74ef
     var grayColor = 0xFF96979a
 
-    val modifier = Modifier
-        .fillMaxWidth()
-        .background(Color.White) // Fond blanc
-    MaterialTheme(
-        colorScheme = colorScheme.copy(primary = Color(0xFF1E88E5)) ,
-        typography = MaterialTheme.typography,
-        shapes = shapes
-    ) {
-        // Contenu du bandeau
+    CarbonalyserTheme {
         Column(
-            modifier = modifier
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
                 .padding(16.dp)
                 .fillMaxWidth()
                 .wrapContentHeight(Alignment.CenterVertically)
@@ -47,29 +38,25 @@ fun Header() {
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalAlignment = Alignment.Start
         ) {
-            // Logo de l'application et nom collés
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "Logo de l'application",
+                    contentDescription = "Application logo",
                     modifier = Modifier
                         .size(48.dp)
                         .clip(CircleShape)
                 )
-
-                // Nom de l'application
                 Text(
                     text = "Nom de l'application",
-                    color = Color.Black, // Couleur bleue
+                    color = Color.Black,
                     fontFamily = FontFamily(Font(R.font.aleo_bold)),
-                    fontSize = 20.sp // Taille de la police
+                    fontSize = 20.sp
                 )
             }
 
-            // Raccourcis vers les pages prenant toute la largeur
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
