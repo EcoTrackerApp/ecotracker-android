b2ab854fdbf6f88c55f35cc1073eddff2e5ce1a6
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-03-14 17:38:45 +0100

    delete useless stuff

--- app/src/main/java/fr/umontpellier/carbonalyser/data/model/EmissionData.kt
+++
@@ -1,5 +0,0 @@
-package fr.umontpellier.carbonalyser.data.model
-
-import java.util.Date
-
-data class EmissionData(val date: Date, val co2Emissions: Double)
--- app/src/main/java/fr/umontpellier/carbonalyser/data/repository/EmissionsRepository.kt
+++
@@ -1,7 +0,0 @@
-package fr.umontpellier.carbonalyser.data.repository
-
-import fr.umontpellier.carbonalyser.data.model.EmissionData
-
-class EmissionsRepository {
-    // TODO: Implement the logic to get emission data, e.g. from a database or an API
-}
