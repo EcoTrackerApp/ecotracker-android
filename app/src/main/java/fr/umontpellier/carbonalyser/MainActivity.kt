56d537293b1849b96331808da2ff4b7eb9fa3fdd
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-03-15 17:11:48 +0100

    begin charts

--- app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import fr.umontpellier.carbonalyser.ui.theme.CarbonalyserTheme
-import kotlinx.coroutines.launch
-import java.time.Instant
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CarbonalyserTheme {
-            }
         }
     }
 }
