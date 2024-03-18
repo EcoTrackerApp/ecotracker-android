de50e435acd4629c9a198c86e76d8ea45a99a6ad
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2024-03-18 14:44:24 +0100

    ça avance

--- app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/MainActivity.kt
@@ -3,11 +3,15 @@ package fr.umontpellier.carbonalyser
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import fr.umontpellier.carbonalyser.ui.components.dataGraph.MonthlyData
+import fr.umontpellier.carbonalyser.ui.components.dataGraph.generateRandomDataForYear
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val randomData = generateRandomDataForYear(2024)
+            MonthlyData(randomData.first, randomData.second)
         }
     }
 }
