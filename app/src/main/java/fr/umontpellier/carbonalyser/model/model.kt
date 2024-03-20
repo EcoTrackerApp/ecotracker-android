b78f61fa37c89572ad867beab9a770958fb1bb4b
VIAL Sébastien <sebvi66@hotmail.fr>
2024-03-20 11:41:13 +0100

    feat(model): abstraction and service

--- app/src/main/java/fr/umontpellier/carbonalyser/model/model.kt
+++ app/src/main/java/fr/umontpellier/carbonalyser/model/model.kt
@@ -4,6 +4,16 @@ import fr.umontpellier.carbonalyser.android.PackageNetworkStats
 
 interface Model {
 
-    fun estimate(connectipkgNetStats: PackageNetworkStats)
+    /**
+     * Returns a unique ID for that model.
+     */
+    val id: String
+
+    /**
+     * Estimates the emissions of CO2 from a [PackageNetworkStats] using this model.
+     */
+    fun estimate(connectipkgNetStats: PackageNetworkStats): ModelResult
 
 }
+
+data class ModelResult(val bytesSentEmission: Long, val bytesReceivedEmission: Long)
