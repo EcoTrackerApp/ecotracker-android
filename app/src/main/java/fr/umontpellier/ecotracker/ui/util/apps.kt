336ac137e0f21c2a33e3a1a330de5f53e2a1b1ba
Selj <samy.el-jaafari@etu.umontpellier.fr>
2024-04-30 13:46:38 +0200

    feat(PkgManager): ajout au service d'une fonction pour recuperer l'icone d'une app a partir de son uid

--- app/src/main/java/fr/umontpellier/ecotracker/ui/util/apps.kt
+++ app/src/main/java/fr/umontpellier/ecotracker/ui/util/apps.kt
@@ -1,10 +1,30 @@
 package fr.umontpellier.ecotracker.ui.util
 
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
+import fr.umontpellier.ecotracker.R
 
 fun PackageManager.getPackageName(uid: Int): String {
     return getPackagesForUid(uid)
         ?.map { pkg -> getPackageInfo(pkg, PackageManager.GET_META_DATA) }
-        ?.joinToString(", ") { info -> getApplicationLabel(info.applicationInfo) }
+        ?.map { info -> getApplicationLabel(info.applicationInfo).toString() }
+        ?.let { labels ->
+            if (labels.size > 3) {
+                labels.take(3).joinToString(", ") + "..."
+            } else {
+                labels.joinToString(", ")
+            }
+        }
         ?: "Système"
+}        // BarChart occupant 2/3 de l'écran en hauteur
+
+fun PackageManager.getAppIcon(uid: Int): Drawable? {
+    val packages = getPackagesForUid(uid)
+
+    packages?.firstOrNull()?.let { packageName ->
+        val appInfo = getApplicationInfo(packageName, 0)
+        return appInfo.loadIcon(this)
+
+    }
+    return null
 }
